ff909db3eafa1a4fb5fba7f4513500ed26e71a87
Rafał Łempa <rafal.lempa@tivix.com>
2022-11-06 13:19:57 +0100

    Add date formats to config

--- config/config.php
+++ config/config.php
@@ -8,4 +8,9 @@ return [
     |
     | This file is for storing configurations related to tools.
     */
+    'date' => [
+        'format' => env('KELLTON_TOOLS_DATE_FORMAT', 'Y-m-d'),
+        'time_format' => env('KELLTON_TOOLS_TIME_FORMAT', 'H:i:s'),
+        'datetime_format' => env('KELLTON_TOOLS_DATE_TIME_FORMAT', 'Y-m-d H:i:s'),
+    ],
 ];
