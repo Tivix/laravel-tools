8f247a9b297cf3705e93add86ed76850232b3e9a
kBieda <konrad.bieda@tivix.com>
2022-12-05 13:06:03 +0100

    Feature: Refactor models Add base Pivot Model

---
+++ src/Models/ModelInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Kellton\Tools\Models;
+
+use Illuminate\Database\Eloquent\Model as EloquentModel;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Kellton\Tools\Builders\Builder;
+use Kellton\Tools\Models\Traits\HasTableName;
+
+/**
+ * ModelInterface handles the model interface.
+ */
+interface ModelInterface
+{
+}
